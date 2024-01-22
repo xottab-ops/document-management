e23b8f6b9c3e663799a697c2484053bc15dd4c34
jassik23 <jassik2002@mail.ru>
2024-01-23 01:40:02 +1000

    Fixed the edits of SAST tools (flake8, pylint)

--- final_project_django/authorization.py
+++ final_project_django/authorization.py
@@ -1,3 +1,26 @@
+"""
+Custom JWT Authentication Module for Django Rest Framework
+with Keycloak Integration.
+
+This module enables seamless integration between Django Rest Framework (DRF)
+andKeycloak for JSON Web Token (JWT) based user authentication.
+Users are authenticatedusing JWT tokens issued by Keycloak,
+and the module provides flexibility in
+configuration for easy adaptation to different Keycloak setups.
+
+Key Features:
+- Utilizes JWT tokens for user authentication.
+- Integrates with Keycloak for secure and reliable authentication.
+- Maps Keycloak roles to Django user permissions.
+
+Usage:
+1. Install the module in your Django project.
+2. Configure Keycloak server details in your Django settings.
+3. Add the custom authentication class to the DRF authentication classes.
+
+
+"""
+import os
 import jwt
 from django.utils import timezone
 from drf_spectacular.extensions import (
@@ -9,16 +32,21 @@ from rest_framework import exceptions
 
 from users.models import User
 
-import os
 
-client_secret = ""
+CLIENT_SECRET = ""
 ALGORITHM = "HS256"
 
 
 class KeycloakAuthenticationBackend(BaseAuthentication):
+    """
+    The class of authorization module that
+    inherits from the BaseAuthentication module
+    """
+
     keyword = "Bearer"
-    client_secret = os.getenv(
-        "KEYCLOAK_CLIENT_SECRET", "TAKE YOUR SECRET HERE TO LOCAL PERMISSION TO YOUR KEYCLOAK"
+    CLIENT_SECRET = os.getenv(
+        "KEYCLOAK_CLIENT_SECRET",
+        "TAKE YOUR SECRET HERE TO LOCAL PERMISSION TO YOUR KEYCLOAK",
     )
 
     def authenticate(self, request, username=None, password=None):
@@ -45,15 +73,20 @@ class KeycloakAuthenticationBackend(BaseAuthentication):
             token = str.replace(token, "Bearer ", "")
             return jwt.decode(
                 jwt=token,
-                key=client_secret,
+                key=CLIENT_SECRET,
                 algorithms=[ALGORITHM],
                 options={"verify_signature": False},
             )
-        except Exception:
+        except jwt.exceptions.ExpiredSignatureError:
             return None
 
 
 class KeycloakAuthenticationScheme(OpenApiAuthenticationExtension):
+    """
+    An authorization module class that creates
+    an authorization scheme for drf_spectacular
+    """
+
     target_class = "final_project_django.authorization.KeycloakAuthenticationBackend"
     name = "BearerToken"
     priority = -1
